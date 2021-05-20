a60384ff4073c200f26ac598873ff82a0f3852e2
Luis Rujana <lmrujanass@gmail.com>
2021-05-19 23:16:46 -0400

    Created All Models

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,18 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int number1 = 10;
+        double number2 = 12.25;
+
+        double sum = number1 + number2;
+
+        String myName = "Luis";
+        String concat = myName + number1;
+
+        System.out.println(sum);
+        //sum is type double
+        System.out.println(concat);
+        //concat is type String
+    }
+}
